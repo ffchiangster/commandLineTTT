15e2df4f89f12852d2855480f2c749aa747e50b0
Fiona Chiang <ffchiangster@gmail.com>
2017-01-23 15:28:23 -0800

    Added comments

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,6 +1,6 @@
-var prompt = require('prompt');
+let prompt = require('prompt');
 
-var board = {
+let board = {
   1: ' ',
   2: ' ',
   3: ' ',
@@ -13,18 +13,23 @@ var board = {
 };
 
 var markPosition = function (position, piece) {
-  board[position] = piece.toUpperCase();
-};
+  board[position] = piece.toUpperCase(); // set the piece to be the value of the position that the user inputs.
+}; // need to mark the positions somehow?
 
-var newBoard = function () {
+let newBoard = function () { //depending on what the board is, it'll create a board with the keys in the object of board.
   console.log(
     '\n' +
-      ' ' + board[1] + ' 1  | ' + board[2] + '2  | ' + board[3] + '3' + '\n' +
+      ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
       ' -----------------\n' +
-      ' ' + board[4] + ' 4  | ' + board[5] + '5  | ' + board[6] + '6' + '\n' +
+      ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
       ' -----------------\n' +
-      ' ' + board[7] + ' 7  | ' + board[8] + '8  | ' + board[9] + '9' + '\n'
-  );
+      ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n'
+  ); //create a board using keys in board object
 };
+
+let turn = function(player) {
+  console.log('Your turn to play' + player);
+}; // establish if turn is X or O, if turn is X, mark the position that player X inputs
+// then change the turn to 'O' player and update board.
 
 newBoard();

5eef4b64660793523b5951589e7a3f6920717606
Fiona Chiang <ffchiangster@gmail.com>
2017-01-24 09:49:33 -0800

    Added grid references

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,15 +1,15 @@
 let prompt = require('prompt');
 
 let board = {
-  1: ' ',
-  2: ' ',
-  3: ' ',
-  4: ' ',
-  5: ' ',
-  6: ' ',
-  7: ' ',
-  8: ' ',
-  9: ' '
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '8',
+  9: '9'
 };
 
 var markPosition = function (position, piece) {
@@ -20,9 +20,9 @@ let newBoard = function () { //depending on what the board is, it'll create a bo
   console.log(
     '\n' +
       ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
-      ' -----------------\n' +
+      ' ----------\n' +
       ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
-      ' -----------------\n' +
+      ' ----------\n' +
       ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n'
   ); //create a board using keys in board object
 };
